6e56f3f461d0978fcf4b649141c0f0faddac7505
phucnhgch18652 <65522550+phucnhgch18652@users.noreply.github.com>
2022-05-25 11:15:18 +0700

    first commit

---
+++ day1/hello.js
@@ -0,0 +1,32 @@
+// console.log("░░░░░███████ ]▄▄▄▄▄▄▄▄▃ Đm khuiPEO nháá á á á...")
+// console.log("▂▄▅█████████▅▄▃▂")
+// console.log("I███████████████████].")
+// console.log(" ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...")
+
+// function xinChao(ten){
+//     console.log("Hello " + ten)
+// }
+// var ten1 = "khuiPEO1"
+// xinChao("KhuiPeo2")
+// for(i = 1; i < 4; i++){
+//     xinChao(ten1)
+// }
+// for(i=0;i<ten1.length;i++){
+//     console.log(ten1[i] +'')
+// }
+// let ten2 = "khui|qua|beo|$123"
+// for(i=0;i<ten2.length;i++){
+//     if(isNaN(ten2[i])){
+//         console.log(ten2[i] + ' not a number')
+//     }else{
+//         console.log(ten2[i] + ' la so')
+//     }
+// }
+// console.log(isNaN("khuipeo123"))
+
+let color ="xanh,hong,tim"
+let mang = color.split(",")
+for(i=0;i<mang.length;i++)
+{
+    console.log(mang[i].toUpperCase())
+}
---
+++ day1/myserver.js
@@ -0,0 +1,20 @@
+var http = require('http')
+
+var myserver = http.createServer(function(req,res){
+    res.writeHead(200, { 'Content-Type': 'text/html' }); 
+    if (req.url=='/home'){
+        res.write('<html><body><p style="color:red">name: <input type="text"></input> </p></body></html>')
+        res.write('<html><body><p style="color:blue">Gender: <input type="checkbox"></input> <label>male</label> <input type="checkbox"></input> <label>female</label> </p></body></html>')
+        res.write('<html><body><p style="color:green">country: <label for="country"> choose country </label> <select id="country" name="countrylist" form="countryform"> <option value="usa">usa </option> <option value="Viet Nam">Viet Nam </option>  </select> </p></body></html>')
+        res.write('<html><body><p style="color:black"><input type="submit" value="ok"></input> </p></body></html>')
+        res.end();
+    }  else if (req.url =='/student'){
+        res.write('<html><body><p style="color:blue">SUDENT CHING CHONG HANJI</p></body></html>')
+        res.end();
+    } else{
+        res.write('<html><body><p style="color:blue">CHING CHONG HANJI</p></body></html>')
+    }res.end();
+})
+
+myserver.listen(3000)
+console.log("Server is running!")
