21529ede1dc20df489ef73e86e6a79379bea252c
phucnhgch18652 <65522550+phucnhgch18652@users.noreply.github.com>
2022-06-01 08:05:29 +0700

    new2

--- day1/myserver2.js
+++ day1/myserver2.js
@@ -2,9 +2,12 @@ var http = require('http')
 var fs = require('fs')
 
 var myserver2 = http.createServer(function(req,res){
-    fs.readFile('mydata.txt','utf8',function(err,data){
+    fs.readFile('mydata.html',function(err,data){
         if(!err){
             res.write(data)
+            res.end()
+        }else{
+            console.log("something was wrong")
             res.end()
         }
     })
